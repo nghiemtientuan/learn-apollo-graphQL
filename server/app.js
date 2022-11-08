40107b1d069e812ddf87854ed56e5620b596bb29
Nghiem Tuan <nghiem.tuan@tda.company>
2022-11-08 18:44:32 +0700

    init

--- server/app.js
+++ server/app.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const {ApolloServer} = require('apollo-server-express')
+const mongoose = require('mongoose')
 
 // Load schema & resolvers
 const typeDefs = require('./schema/schema')
 const resolvers = require('./resolver/resolver')
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://nghiemtuan:Aa@123456@graphql.zqtqqie.mongodb.net/?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    console.log('MongoDB connected!!!')
+  } catch (error) {
+    console.log(error.message)
+    process.exit(1)
+  }
+}
+
 async function startServer() {
+  await connectDB()
   const server = new ApolloServer({
     typeDefs,
     resolvers
